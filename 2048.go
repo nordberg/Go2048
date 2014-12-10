4b44c7321b08b46e82185609f59ce2cb2e669d60
nordberg <marcusnordberg@live.se>
2014-12-10 20:51:12 +0100

    Printing board, input via U, D, L, R (doesnt update the board yet)

--- 2048.go
+++ 2048.go
@@ -5,10 +5,20 @@ import (
     "math/rand"
     "time"
     "container/list"
+    "strconv"
+    "bufio"
+    "os"
+    "strings"
 )
 
 type Tuple struct {
-    a, b interface{}
+    a, b int
+}
+
+type key struct {
+    x  int
+    y  int
+    ch rune
 }
 
 const (
@@ -16,8 +26,17 @@ const (
     height = 4
 )
 
+var K_ARROW_UP = []key{{54, 10, '('}, {55, 10, 0x2191}, {56, 10, ')'}}
+var K_ARROW_LEFT = []key{{50, 12, '('}, {51, 12, 0x2190}, {52, 12, ')'}}
+var K_ARROW_DOWN = []key{{54, 12, '('}, {55, 12, 0x2193}, {56, 12, ')'}}
+var K_ARROW_RIGHT = []key{{58, 12, '('}, {59, 12, 0x2192}, {60, 12, ')'}}
+
 func main() {
+    gameOver := false
+    score := 0
+
     fmt.Println("Welcome to 2048!")
+    fmt.Println("R: Right, U: Up, D: Down, L: Left (lower case works)")
 
     //round := 1
     board := make([][]int, height)
@@ -26,23 +45,40 @@ func main() {
         board[i] = make([]int, width)
     }
 
-    addTwo(board)
+    for !gameOver {
+        if !addTwo(board) {
+            break
+        }
+
+        fmt.Println(printBoard(board))
+
+        userInput(board)
+    }
+
+    fmt.Println("Game over! Score: ", score)
+
+    
 }
 
-func addTwo(board [][]int) {
+func addTwo(board [][]int) bool {
     r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-    openSpots := list.New()
-
-    board[0][1] = 2
+    // A list with possible spots for the new number. If length = 0 -> Game Over
+    openSpots := list.New();
 
     for i := range board {
         for j := range board[i] {
+            // If empty, add to openSpots list
             if board[i][j] == 0 {
+                //tupl := []int{i, j}
                 tupl := Tuple{i, j}
                 openSpots.PushFront(tupl)
             }
         }
+    }
+
+    if (openSpots.Len() == 0) {
+        return false
     }
 
     index := r.Intn(openSpots.Len())
@@ -50,10 +86,50 @@ func addTwo(board [][]int) {
     e := openSpots.Front()
     for i := 0; i < openSpots.Len(); i++ {
         if i == index {
-            fmt.Println(e.Value)
-            return
+            var a int = e.Value.(Tuple).a
+            var b int = e.Value.(Tuple).b
+            board[a][b] = 2
+            return true
         }
+
         e = e.Next()
     }
+
+    return true
 }
 
+func userInput(board [][]int) {
+    reader := bufio.NewReader(os.Stdin)
+    input, _ := reader.ReadString('\n')
+
+    input = strings.ToLower(input)
+
+    switch input {
+    case "u":
+        
+    case "d":
+
+    case "l":
+
+    case "r":
+
+    default:
+
+    }
+
+}
+
+func printBoard(board [][]int) string {
+
+    var output string
+
+    for i := 0; i < width; i++ {
+        for j := 0; j < height; j++ {
+            output += "[" + strconv.Itoa(board[i][j]) + "]"
+            //output += "[ " + strconv.Itoa(board[i][j]) + " ]"
+        }
+        output += "\n"
+    }
+
+    return output
+}
